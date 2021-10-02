6ebca42bf7e31d661a7fbe4078ac811c8db53934
chintansuthar72 <mrchintansuthar@gmail.com>
2021-10-02 14:42:23 +0530

    added delete route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var users; // will store all users
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// delete order
+router.route('/delete/:id').delete((req,res) => {
+  users.foreach((user,idx) =>{ // loop over all orders and find order with given id
+    if(user.id == req.params.id){
+      users.remove(order[idx]);
+      res.json("Order deleted!")
+    }
+  });
+})
+
 
 module.exports = router;
