fe46d0836adedc6b2ec6c681c9cb9ccca996ef1f
Prateek Saini <pratiiksaini@gmail.com>
2021-10-02 19:55:52 +0530

    Update users.js
    Fix the User route code
    1. rewrite the get all orders api.
    2. rewrite the push api.
    3. rewrite the update api.
    4. rewrite the delete api.

--- routes/users.js
+++ routes/users.js
@@ -2,87 +2,122 @@ var express = require("express");
 var router = express.Router();
 const Order = require("../models/food.model");
 
-/* GET users listing. */
 //Get all the orders
 router.get("/", function (req, res, next) {
-  const allOrder = Order.find({})
-    .then((order) => {
-      res.json({ order });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-//Add an Order
-router.post("/", function (req, res, next) {
-  const { food, customerName, foodQuantiy } = req.body;
-  //check for empty value
-  if (!food || !customerName || !foodQuantiy) {
-    return res.send("Enter all details");
-  }
-  const order = new Order({
-    food: food,
-    customerName: customerName,
-    quantity: foodQuantiy,
-  })
-    .save()
-    .then((myorder) => {
-      res.json({ myorder });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-// delete order - MongoDB version
-router.route('/delete/:id').delete((req,res) => {
-  Order.findByIdAndDelete(req.params.id)
-  .then(() => res.json("Order deleted!"))
-  .catch(error => res.status(400).json('Error : ' + error)); // If there is no order with requested id then throw error (Bad request)
-})
-
-// update order - MongoDB version
-router.route('/update/:id').post((req,res) => {
-    
-  Order.findById(req.params.id)
-  .then(order => {
-      order.food = req.body.food; 
-      order.customerName = req.body.customerName;
-      order.quantity = req.body.quantity;
-      
-      order.save()
-      .then(() => res.json("Order updated!"))
-      .catch(error => res.status(200).json('Error : ' + error)); // This indicates that request is succeeded!
-  })
-  .catch(error => res.status(400).send('Error : ' + error)); // If there is no user with requested id then throw error (Bad request)
-})
-
-
-// delete order
-router.route('/delete/:id').delete((req,res) => {
-  users.foreach((user,idx) =>{ // loop over all orders and find order with given id
-    if(user.id == req.params.id){
-      users.remove(order[idx]);
-      res.json("Order deleted!")
-    }
-  });
-})
-
-
-
-// update order
-router.route('/update/:id').post((req,res) => {
-    
-  users.foreach((user,idx) =>{ // loop over all orders and find order with given id
-    if(user.id == req.params.id){
-      // merge object with requested updation
-      users[idx] = {
-        ...users[idx],
-        ...(req.body)
+  var sort = {};
+  sort['_id'] = -1; 
+  Order.find({}).sort(sort).exec( function(err, orders){
+    if(err){
+      return res.status(400).send({
+        status: 0,
+        message: "Something went wrong"
+      })
+    } else{
+      if (orders.length) {
+        return res.json({
+          status: 1,
+          "Total Records": orders.length,
+          data: orders
+        });
       }
-      res.json("Order updated!")
+      return res.status(200).send({
+        status: 1,
+        message: 'No Data found'
+      })
     }
-  });
+  })
 })
+
+//Push an Order
+router.post("/", function (req, res, next) {
+  var data = req.body;
+  //check for empty value
+  if (!data.food || !data.customerName || !data.quantity) {
+    return res.status(400).json({
+      status: 0,
+      message: 'enter all necessary details'
+    })
+  }
+  var orderData = new Order(data);
+  orderData.save(function(err, result){
+    if(err){
+      return res.status(400).json({
+        status: 0,
+        message: 'something went wrong'
+      })
+    } else{
+      res.json({
+        status: 1,
+        "message": "Order Added successfully",
+        data: result
+    });
+    }
+  })
+})
+
+//Delete an Order
+router.route('/delete/:id').delete((req,res) => {
+  var orderId = req.params.id;
+  Order.findOneAndDelete({_id: orderId}).exec(function(err, order){
+    if (err) {
+      return res.status(400).send({
+          status: 0,
+          message: 'something went wrong'
+      })
+    } else{
+      res.json({
+        status: 1,
+        message: "Order deleted successfully",
+        "data": order
+      })
+    }
+  })
+})
+
+//Update an Order
+router.route('/update/:id').post((req,res) => {   
+  var orderId = req.params.id;
+  var data = req.body;
+  Order.findOne({_id: orderId}).exec(function(err, order){
+    if (err) {
+      return res.status(400).send({
+          status: 0,
+          message: 'Order Id is not correct'
+      })
+    } else{
+      if(order){
+        if (data.food) {
+          order.food = data.food
+        }
+        if (data.customerName) {
+          order.customerName = data.customerName;
+        }
+        if (data.quantity) {
+          order.quantity = data.quantity;
+        }
+        order.save(function (err, result) {
+          if (err) {
+            console.log("error----------", err);
+            return res.status(400).send({
+              status: 0,
+              message: err
+            })
+          } else{
+            res.json({
+              status: 1,
+              "message": "Order updated successfully",
+              data: result
+            });            
+          }
+        })
+      } else {
+        return res.json({
+          status: 0,
+          message: 'No Order Stored with this Id '
+        })
+      }
+    }
+  })
+})
+
 module.exports = router;
