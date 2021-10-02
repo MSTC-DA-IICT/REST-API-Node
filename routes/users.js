cb53a2a787a30ffd3795c0fd373c35c298431114
slowgeek <kumarritesh14062000@gmail.com>
2021-10-02 17:26:02 +0530

    Read and Create Route

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,37 @@
 var express = require("express");
 var router = express.Router();
-//variable to store data
-const user = [];
+const Order = require("../models/food.model");
 
 /* GET users listing. */
+//Get all the orders
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  const allOrder = Order.find({})
+    .then((order) => {
+      res.json({ order });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+//Add an Order
 router.post("/", function (req, res, next) {
   const { food, customerName, foodQuantiy } = req.body;
   //check for empty value
   if (!food || !customerName || !foodQuantiy) {
     return res.send("Enter all details");
   }
-  user.push({ id: user.length, food, customerName, foodQuantiy });
-  console.log(user);
-  return res.send("Order Taken");
+  const order = new Order({
+    food: food,
+    customerName: customerName,
+    quantity: foodQuantiy,
+  })
+    .save()
+    .then((myorder) => {
+      res.json({ myorder });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
