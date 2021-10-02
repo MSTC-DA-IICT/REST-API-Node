381c0135ce917a2e822d72160f7a129812192f46
slowgeek <kumarritesh14062000@gmail.com>
2021-10-02 15:59:40 +0530

    Create Route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ message: "connected" });
+
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+//variable to store data
+const user = [];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+router.post("/", function (req, res, next) {
+  const { food, customerName, foodQuantiy } = req.body;
+  //check for empty value
+  if (!food || !customerName || !foodQuantiy) {
+    return res.send("Enter all details");
+  }
+  user.push({ id: user.length, food, customerName, foodQuantiy });
+  console.log(user);
+  return res.send("Order Taken");
 });
 
 module.exports = router;
