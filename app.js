9c5bb91aaae10fdd7bbf2e9a79d8be83008c1de1
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2021-10-15 19:09:37 -0300

    chore: added helmet

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var helmet = require("helmet");
 const mongoose = require("mongoose");
 //Connect to DB
 mongoose.connect("mongodb://localhost:27017/FoodDB", {
@@ -35,6 +35,7 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
