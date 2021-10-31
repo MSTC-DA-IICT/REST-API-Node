d180859c55464ceec274b6c35b1e492891465160
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-10-31 13:08:52 -0400

    unhandled promise from mongo solved

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/FoodDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("connected to mongo");
-});
-
-mongoose.connection.on("error", (err) => {
+}).then(res => {
+  console.log("connected to mongo: ");
+}).catch(err => {
   console.log("error connecting to mongo", err);
 });
 
